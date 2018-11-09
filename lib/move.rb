846e60da679482fda70cd86ea8e64c46fc9c0737
mchoi89 <mchoi89@gmail.com>
2018-11-09 17:55:57 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,3 +8,10 @@ end
 
 # code your input_to_index and move method here!
 #input_to_index("1"-"9")
+
+board = [" ", " ", " "]
+def update_array_at_with(array,index,value)
+  array[index] = value
+end
+
+update_array_at_with(board, 0, "X")
